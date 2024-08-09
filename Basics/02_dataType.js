8ff982617be895d416e1929f767a06a3e32fec07
Gurpal singh <gurusaghi324@gmail.com>
2024-08-09 17:37:30 +0000

    commit

---
+++ Basics/02_dataType.js
@@ -0,0 +1,51 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello guru") // we are using nodejs, not browser
+
+console.log(3 + 3) // code readability should be high
+
+console.log("Guru")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// With decimals:
+let x1 = 34.00;
+
+// Without decimals:
+let x2 = 34;
+
+
+// bigint
+// let x : BigInt("123456789012345678901234567890");
+
+// string => ""
+// Single quote inside double quotes:
+let answer1 = "It's alright";
+
+// Single quotes inside double quotes:
+let answer2 = "He is called 'Johnny'";
+
+// Double quotes inside single quotes:
+let answer3 = 'He is called "Johnny"';
+
+// boolean => true/false
+let x = 5;
+let y = 5;
+let z = 6;
+(x == y)       // Returns true
+(x == z)       // Returns false
+
+// symbol => unique
+
+
+// object
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+// null => standalone value
+// undefined => 
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
