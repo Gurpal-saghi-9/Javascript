84e502588faa2796d459c64b79afc7aacae49aac
guru <gurusaghi324@gmail.com>
2025-03-05 21:52:47 +0530

    commit

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -59,3 +59,32 @@
 // }
 
 // --------------------------------------------------------
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Please enter a number: ', (input) => {
+    let num = Number(input);
+
+    if (num > 0) {
+        console.log("Positive");
+    }
+    else if (num < 0) {    
+        console.log("Negative");
+    }
+    else if (num === 0) {
+        console.log("Zero");
+    }
+    else {
+        console.log("Invalid input");
+    }
+
+    rl.close();
+});
+
+// --------------------------------------------------------
+
