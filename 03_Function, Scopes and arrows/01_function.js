81101da3785f8f05cbe1b00fb21f9a4db0f352f3
guru <gurusaghi324@gmail.com>
2025-03-05 19:10:17 +0530

    commit

--- 03_Function, Scopes and arrows/01_function.js
+++ 03_Function, Scopes and arrows/01_function.js
@@ -1,70 +1,26 @@
-
-function sayMyName(){
-    console.log("H");
-    console.log("I");
-    console.log("T");
-    console.log("E");
-    console.log("S");
-    console.log("H");
+function MyName() {
+    console.log("G");
+    console.log("U");
+    console.log("R");
+    console.log("P");
+    console.log("A");
+    console.log("L");
 }
 
-// sayMyName()
+// MyName();
 
-// function addTwoNumbers(number1, number2){
-
-//     console.log(number1 + number2);
-// }
-
-function addTwoNumbers(number1, number2){
-
-    // let result = number1 + number2
-    // return result
-    return number1 + number2
+function Addition(number1, number2){
+    let result = number1 + number2;
+    return result;
 }
 
-const result = addTwoNumbers(3, 5)
-
-// console.log("Result: ", result);
-
-
-function loginUserMessage(username = "sam"){
-    if(!username){
-        console.log("PLease enter a username");
-        return
-    }
-    return `${username} just logged in`
+function Addition(number1, number2){
+    return number1 + number2;
 }
+// let sum = Addition(10, 20);
+// console.log("Result :", sum);
 
-// console.log(loginUserMessage("hitesh"))
-// console.log(loginUserMessage("hitesh"))
-
-
-function calculateCartPrice(val1, val2, ...num1){
-    return num1
+function Addition(number1, number2){
+    console.log("Sum of ", number1, " and ", number2, " is ", number1 + number2);
 }
-
-// console.log(calculateCartPrice(200, 400, 500, 2000))
-
-const user = {
-    username: "hitesh",
-    prices: 199
-}
-
-function handleObject(anyobject){
-    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
-}
-
-// handleObject(user)
-handleObject({
-    username: "sam",
-    price: 399
-})
-
-const myNewArray = [200, 400, 100, 600]
-
-function returnSecondValue(getArray){
-    return getArray[1]
-}
-
-// console.log(returnSecondValue(myNewArray));
-console.log(returnSecondValue([200, 400, 500, 1000]));
+// Addition(30, 20);
