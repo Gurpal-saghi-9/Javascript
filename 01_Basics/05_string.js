719264428c8b2cb9db63eaa64262a4460c38570b
guru <gurusaghi324@gmail.com>
2025-03-05 15:28:24 +0530

    commit

--- 01_Basics/05_string.js
+++ 01_Basics/05_string.js
@@ -1,7 +1,7 @@
-const name = "Gurpal"
-const repoCount = 50
+const name = "Gurpal";
+const repoCount = 50;
 
-// console.log(name + repocount + "value")
+// console.log(name + repoCount + "value")
 
 // console.log(`hello my name is ${name} Singh. My repo count is ${repoCount}`);
 
@@ -17,20 +17,20 @@ const gameName = new String('Gurpal_GS_com')
 // console.log(gameName.indexOf('t')); // -1 
 
 const newString = gameName.substring(0, 4)
-// console.log(newString);
+// console.log(newString); // Gurp
 
 const anotherString = gameName.slice(-7, 10)
 // console.log(anotherString); //_GS_
 
 const newStringOne = "   Gurpal    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// console.log(newStringOne); //    Gurpal    
+// console.log(newStringOne.trim()); //Gurpal
 
 
 const url = "https://www.google.com%20"
 
-console.log(url.replace('%20', '-')) //https://www.google.com-
+// console.log(url.replace('%20', '-')) //https://www.google.com-
 
-console.log(url.includes('www')) // true
+// console.log(url.includes('www')) // true
 
-console.log(gameName.split('_')); // [ 'Gurpal_GS_com' ]
+// console.log(gameName.split('_')); // [ 'Gurpal_GS_com' ]
