fa77f326be0486b34a358eab47c97add9618a7cc
guru <gurusaghi324@gmail.com>
2025-03-05 16:19:50 +0530

    commit

--- 02_Arrays and Objects/01_array.js
+++ 02_Arrays and Objects/01_array.js
@@ -8,32 +8,41 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // Array methods
 
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+myArr.push(6)
+// console.log(myArr);
+myArr.push(7)
+// console.log(myArr);
+myArr.pop()
+// console.log(myArr);
 
-// myArr.unshift(9)
-// myArr.shift()
+// myArr.unshift(0)
+// console.log(myArr);
+
+myArr.unshift(9)
+// console.log(myArr);
+
+myArr.shift()
+// console.log(myArr);
 
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()
+const newArr = myArr.join()
 
 // console.log(myArr);
 // console.log( newArr);
 
 
-// slice, splice
+// // slice, splice
 
-console.log("A ", myArr);
+// console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3)
 
-console.log(myn1); 
-console.log("B ", myArr);
+// console.log(myn1); 
+// console.log("B ", myArr);
 
 
 const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+// console.log("C ", myArr);
+// console.log(myn2);
