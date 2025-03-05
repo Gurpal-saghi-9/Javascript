884a394c1b424f4b2b8b794026c681746198e1c2
guru <gurusaghi324@gmail.com>
2025-03-05 21:57:46 +0530

    commit

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -60,31 +60,48 @@
 
 // --------------------------------------------------------
 
-const readline = require('readline');
+// const readline = require('readline');
 
-const rl = readline.createInterface({
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// rl.question('Please enter a number: ', (input) => {
+//     let num = Number(input);
+
+//     if (num > 0) {
+//         console.log("Positive");
+//     }
+//     else if (num < 0) {    
+//         console.log("Negative");
+//     }
+//     else if (num === 0) {
+//         console.log("Zero");
+//     }
+//     else {
+//         console.log("Invalid input");
+//     }
+
+//     rl.close();
+// });
+
+// --------------------------------------------------------
+
+import readline from 'readline';
+
+const rl2 = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question('Please enter a number: ', (input) => {
+rl2.question('Please enter a number: ', (input) => {
     let num = Number(input);
-
-    if (num > 0) {
-        console.log("Positive");
+    if (num % 2 === 0) {
+        console.log("Even");
+    } 
+    else {    
+        console.log("Odd");
     }
-    else if (num < 0) {    
-        console.log("Negative");
-    }
-    else if (num === 0) {
-        console.log("Zero");
-    }
-    else {
-        console.log("Invalid input");
-    }
-
-    rl.close();
+    rl2.close();
 });
-
-// --------------------------------------------------------
-
