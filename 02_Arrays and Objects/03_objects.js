4d90ec579db1bc16edeb29b3977f405697efc252
guru <gurusaghi324@gmail.com>
2025-03-05 18:00:09 +0530

    commit

--- 02_Arrays and Objects/03_objects.js
+++ 02_Arrays and Objects/03_objects.js
@@ -7,12 +7,12 @@ const mySym = Symbol("key1")
 
 
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
+    name: "Gurpal",
+    "full name": "Gurpal Singh",
     [mySym]: "mykey1",
-    age: 18,
-    location: "Jaipur",
-    email: "hitesh@google.com",
+    age: 19,
+    location: "Sirhind",
+    email: "Guru@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
@@ -22,9 +22,9 @@ const JsUser = {
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
-JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+JsUser.email = "Gurpal@chatgpt.com"
+Object.freeze(JsUser)
+JsUser.email = "Gurpal@microsoft.com"
 // console.log(JsUser);
 
 JsUser.greeting = function(){
