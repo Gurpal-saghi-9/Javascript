d39fcd47496672b27e3b74d3a706947cc0f23117
guru <gurusaghi324@gmail.com>
2025-03-05 19:34:59 +0530

    commit

--- 03_Function, Scopes and arrows/01_function.js
+++ 03_Function, Scopes and arrows/01_function.js
@@ -34,5 +34,34 @@ function LoginUserMessage(userName) {
 }
 
 // console.log(LoginUserMessage("Gurpal"));  // This will work and log: "Gurpal just logged in"
-console.log(LoginUserMessage());          // This will log: "Please enter a username"
+// console.log(LoginUserMessage());          // This will log: "Please enter a username"
 
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 700, 800, 1000))
+
+const user = {
+    userName: "Gurpal",
+    price: 30000,
+}
+
+function handleObject(anyobject){
+    console.log(`User Name: ${anyobject.userName} and Price: ${anyobject.price}`);
+}
+
+// handleObject(user);
+// handleObject({
+//     userName: "sam",
+//     price: 20000,
+// });
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+// console.log(returnSecondValue([300, 700, 900, 1100]));
