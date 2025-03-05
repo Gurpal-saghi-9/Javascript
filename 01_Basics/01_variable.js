d005e1db06999c4c11987cbffd091a2b94549ac1
guru <gurusaghi324@gmail.com>
2025-03-05 13:51:49 +0530

    commit

--- 01_Basics/01_variable.js
+++ 01_Basics/01_variable.js
@@ -1,20 +1,4 @@
-const accountId = 144553
-let accountEmail = "hitesh@google.com"
-var accountPassword = "12345"
-accountCity = "Jaipur"
-let accountState;
-
-// accountId = 2 // not allowed
-
-
-accountEmail = "hc@hc.com"
-accountPassword = "21212121"
-accountCity = "Bengaluru"
-
-console.log(accountId);
-/*
-Prefer not to use var
-because of issue in block scope and functional scope
-*/
-
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+const accountID = 11234;
+let accountEmail = "Guru@git.com";
+var accountPassword = "123456";
+accountCity = "Bangalore";
