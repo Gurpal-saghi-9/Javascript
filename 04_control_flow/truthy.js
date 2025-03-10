c7617613944f2293e9b6d5c154acfaf2fb678746
guru <gurusaghi324@gmail.com>
2025-03-10 22:12:34 +0530

    commit

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,10 +1,10 @@
 const userEmail = []
 
-if (userEmail) {
-    console.log("Got user email");
-} else {
-    console.log("Don't have user email");
-}
+// if (userEmail) {
+//     console.log("Got user email");
+// } else {
+//     console.log("Don't have user email");
+// }
 
 // falsy values
 
@@ -17,11 +17,11 @@ if (userEmail) {
 //     console.log("Array is empty");
 // }
 
-const emptyObj = {}
+// const emptyObj = {}
 
-if (Object.keys(emptyObj).length === 0) {
-    console.log("Object is empty");
-}
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("Object is empty");
+// }
 
 // Nullish Coalescing Operator (??): null undefined
 
