9d3ec2fcecfee380d3f823312a3878ac428afd84
guru <gurusaghi324@gmail.com>
2025-05-19 10:30:15 +0530

    commit

--- Chai aur code/02_Arrays and Objects/tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-ingTwo = function(){
-//     console.log(`Hello JS user, ${this["full name"]}`);
-// }
---
+++ Chai aur code/03_Function, Scopes and arrows/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+r1, number2){
+//     console.log("Sum of ", number1, " and ", number2, " is ", number1 + number2);
+// }
