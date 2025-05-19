cc0b349e443f1a9be7829d6d913ac84c5316b27e
guru <gurusaghi324@gmail.com>
2025-05-19 14:29:07 +0530

    commit

--- Chai aur code/05_iterations/03_three.js
+++ Chai aur code/05_iterations/03_three.js
@@ -6,12 +6,12 @@
 const arr = [1, 2, 3, 4, 5]
 
 for (const num of arr) {
-    //console.log(num);
+    // console.log(num);
 }
 
 const greetings = "Hello world!"
 for (const greet of greetings) {
-    //console.log(`Each char is ${greet}`)
+    // console.log(`Each char is ${greet}`)
 }
 
 // Maps
@@ -34,7 +34,6 @@ const myObject = {
     game2: 'Spiderman'
 }
 
-// for (const [key, value] of myObject) {
-//     console.log(key, ':-', value);
-    
-// }
+for (const [key, value] of Object.entries(myObject)) {
+    console.log(key, ':-', value);
+}
