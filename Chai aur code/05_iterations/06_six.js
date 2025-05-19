9af57044c3080e4192e532016cb33c164470e9fc
guru <gurusaghi324@gmail.com>
2025-05-19 17:10:58 +0530

    commit

--- Chai aur code/05_iterations/06_six.js
+++ Chai aur code/05_iterations/06_six.js
@@ -2,31 +2,33 @@ const coding = ["js", "ruby", "java", "python", "cpp"];
 
 const values = coding.forEach((item) => {
   // console.log(item);
-  return item;
+  return item; // for reach cant return any value
 });
 
-// console.log(values);
+// console.log(values); // undefined because for each can't return any value
 
-const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const series = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const newNums = myNums.filter((num) => {
-  return num > 4;
+const newSeries1 = series.filter((num) => num > 5);
+// console.log(newSeries1);
+
+const newSeries2 = series.filter((num) => {
+  return num > 5;
 });
+// console.log(newSeries2);
 
-// const newNums = []
-
-myNums.forEach((num) => {
-  if (num > 4) {
-    newNums.push(num);
+const newSeries3 = [];
+series.forEach((num) => {
+  if (num > 5) {
+    newSeries3.push(num);
   }
 });
-
-// console.log(newNums);
+// console.log(newSeries3);
 
 const books = [
   { title: "Book One", genre: "Fiction", publish: 1981, edition: 2004 },
   { title: "Book Two", genre: "Non-Fiction", publish: 1992, edition: 2008 },
-  { title: "Book Three", genre: "History", publish: 1999, edition: 2007 },
+  { title: "Book Three", genre: "History", publish: 2000, edition: 2007 },
   { title: "Book Four", genre: "Non-Fiction", publish: 1989, edition: 2010 },
   { title: "Book Five", genre: "Science", publish: 2009, edition: 2014 },
   { title: "Book Six", genre: "Fiction", publish: 1987, edition: 2010 },
@@ -35,9 +37,10 @@ const books = [
   { title: "Book Nine", genre: "Non-Fiction", publish: 1981, edition: 1989 },
 ];
 
-let userBooks = books.filter((bk) => bk.genre === "History");
+let userbooks = books.filter((bk) => bk.genre === "Fiction");
 
-userBooks = books.filter((bk) => {
-  return bk.publish >= 1995 && bk.genre === "History";
-});
-// console.log(userBooks);
+const userBooks = books.filter((bk) => {
+  return bk.genre === "History" && bk.publish >= 2000
+})
+
+console.log(userBooks);
