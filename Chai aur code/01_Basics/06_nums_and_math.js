8af562beddc45b0d02d45a8c29fd92792a713fee
guru <gurusaghi324@gmail.com>
2025-05-19 09:53:57 +0530

    commit

--- Chai aur code/01_Basics/06_nums_and_math.js
+++ Chai aur code/01_Basics/06_nums_and_math.js
@@ -8,12 +8,13 @@ const balance = new Number(100)
 // console.log(balance.toString().length);  // 3
 // console.log(balance.toFixed(1)); // 100.0
 
-const otherNumber = 123.8966
+const otherNumber = 124.8966
 
-// console.log(otherNumber.toPrecision());
+// console.log(otherNumber.toPrecision(3)); // returns a string
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-in')); // 10,00,000
+// console.log(hundreds.toLocaleString('en-us')); // 1,000,000
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
@@ -36,4 +37,4 @@ const hundreds = 1000000
 const min = 10
 const max = 40
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)  // 10
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)  
---
+++ Chai aur code/01_Basics/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+eString('en-in')); // 10,00,000
+// console.log(hundreds.toLocaleS
